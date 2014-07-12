2cb93e88fa6aa086bea1e56656b304628e1e1528
bingyulee <bigcookie.lee@gmail.com>
2014-07-12 10:18:31 +0800

    bigcookie: 重新設置打包的檔案位置，並移除 package 的目錄

--- package/androidPackage.bat
+++
@@ -1,2 +0,0 @@
-copy /Y ..\Main.swf
-call "D:\Resource\AdobeAIRSDK 3.7\bin\adt" -package -target apk-captive-runtime -storetype pkcs12 -keystore ../../androidFlash.p12 -storepass 123456 Ntl-v1.0.0711.apk Main-app-android.xml -extdir "C:/github/flashCommon/ane" -extdir "C:/github/flashCommon/ane/Android" Main.swf icons data
--- package/iosPackage.bat
+++
@@ -1,2 +0,0 @@
-copy /Y ..\Main.swf
-call "D:\Resource\AdobeAIRSDK 4.0 Beta\bin\adt" -package -target ipa-test -provisioning-profile ../../AdobeAIRDevelopmentProfile.mobileprovision -storetype pkcs12 -keystore ../../AdobeAIRDevelopment.p12 -storepass cameo Ntl-v1.0.0711.ipa Main-app-ios.xml -extdir "C:/github/flashCommon/ane"  -extdir "C:/github/flashCommon/ane/iOS" Main.swf Default.png Default@2x.png Default-568h@2x.png icons data
