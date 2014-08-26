7ded24934adbed49c5c4eaf7438df85c52f9f5e6
bingyulee <bigcookie.lee@gmail.com>
2014-08-26 23:08:00 +0800

    bigcookie: 調整版本號

--- androidPackage.bat
+++ androidPackage.bat
@@ -1,1 +1,1 @@
-call "D:\Resource\AdobeAIRSDK 3.7\bin\adt" -package -target apk-captive-runtime -storetype pkcs12 -keystore ../androidFlash.p12 -storepass 123456 Ntl-v1.0.0815.apk Main-app-android.xml -extdir "C:/github/flashCommon/ane" -extdir "C:/github/flashCommon/ane/Android" Main.swf icons data
+call "D:\Resource\AdobeAIRSDK 3.7\bin\adt" -package -target apk-captive-runtime -storetype pkcs12 -keystore ../androidFlash.p12 -storepass 123456 Ntl-v1.0.0822.apk Main-app-android.xml -extdir "C:/github/flashCommon/ane" -extdir "C:/github/flashCommon/ane/Android" Main.swf icons data
